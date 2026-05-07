271c3a4822afbd663171c1e61c53580465c53d72
Brian Monestime <bmonestime@gmail.com>
2026-05-07 14:55:02 -0600

    changed font/added new landing page blurb

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,17 @@ import "./App.css";
 function Home() {
   return (
     <>
-      
       <div className="landingImgContainer">
         <h1 className="welcome text"> Welcome</h1>
         <img src={Rizo} className="landingLogo" alt="Rizo's banner" />
         <p className="landingText">
-          Rizo’s Culinary Arts journey began with a deep appreciation for the
-          foundations of French cooking, where she developed discipline,
-          technique, and a respect for quality ingredients. Over the years, she
-          gained experience in a variety of culinary roles, but no matter where
-          her path took her, baking remained her true passion. What started as a
-          simple love for creating bread and sweet treats at home quickly grew
-          into something more. Rizo began sharing her baked goods with family,
-          friends, and neighbors—refining her craft with every batch and
-          building a reputation for warmth, flavor, and consistency. Today, her
-          work reflects both her classical training and her genuine love for
-          baking—bringing people together through thoughtfully made, homemade
-          creations.
+          Rizo finds joy in the quiet magic of baking, where simple ingredients
+          become something warm and meaningful. With every loaf she kneads and
+          every batch she pulls from the oven, she pours a little bit of herself
+          into the process—patience, care, and a genuine desire to make others
+          smile. For her, it’s never just about bread; it’s about the comfort it
+          brings, the conversations it sparks, and the happiness she shares one
+          loaf at a time.
         </p>
       </div>
 
@@ -44,7 +38,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="order" element={<Order />} />
           <Route path="menu" element={<Menu />} />
-          <Route path ="muffinofthemonth" element ={<MuffinOfTheMonth/>}/>
+          <Route path="muffinofthemonth" element={<MuffinOfTheMonth />} />
         </Routes>
       </main>
       <Footer />
